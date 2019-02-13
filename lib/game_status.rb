9717229687642cc279b2ee30329174a9519c4822
Isaiah Magett <imagett10@gmail.com>
2019-02-13 01:42:10 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,4 +54,25 @@ def draw?(board)
   elsif !won?(board) && !full?(board) || !won?(board)
     return false
   end
+  
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
 end
